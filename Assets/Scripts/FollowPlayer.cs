9df8ac9ca6eaf2b99ef45ed9a25a1fe7b0018519
tepbuphata.kim <tepbuphata.kim@axivit.com>
2022-12-27 17:52:58 +0700

    Bonus Features 1 - Hard

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 5, -8);
+    private Vector3 offsetBehindCar = new Vector3(0, 5, -8);
+    private Vector3 offsetInCar = new Vector3(0, 2.18f, 1);
+    private Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
-        
+        offset = offsetBehindCar;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            offset = (offset == offsetBehindCar) ? offsetInCar : offsetBehindCar;
+        }
         transform.position = player.transform.position + offset;
+        if (offset == offsetInCar)
+        {
+            transform.rotation = player.transform.rotation;
+        }
     }
 }
